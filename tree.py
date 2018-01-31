4f16e5c2f865c346a8edfea55e32b487c4d3f2c9
wwt <wwt.cpp@gmail.com>
2018-01-31 11:25:15 +0800

    find expansions of abbreviations from their declerations

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 path = "../mkdata/cocos2d-master-result"
 
@@ -115,8 +116,65 @@ def add_new_child(parent_tree):
     parent_tree.child.append(new_tree)
     return new_tree
 
+def get_abbr_expansion_for_word(abbr_word, origin):
+    # check if number
+    try:
+        int(abbr_word)
+    except:
+        pass
+    else:
+        return [abbr_word]
 
-def tree_split(data, parent_tree, vocabulary, key = None):
+    # check if is arconym
+    expansion = []
+    l = 0
+    for c in abbr_word:
+        while l < len(origin) and origin[l][0] != c:
+            l += 1
+        if l >= len(origin):
+            break
+        expansion.append(origin[l])
+        l += 1
+    else:
+        return expansion
+    
+    # check if is prefix of some word
+    for word in origin:
+        if word.startswith(abbr_word):
+            return [word]
+
+    # check if is just abbreviation of the word (some consonants of the word)
+    for c in abbr_word:
+        if c in ['a', 'e', 'i', 'o', 'u']:
+            break
+    else:
+        for word in origin:
+            l = 0
+            for c in abbr_word:
+                l = word.find(c, l)
+                if l == -1:
+                    break
+                l += 1
+            else:
+                return [word[word.find(abbr_word[0]):]]
+
+    return [abbr_word]
+
+def get_abbr_expansion(abbr, origin):
+    abbr = combine_name_split(abbr)
+    origin = combine_name_split(origin)
+    #print(abbr, origin, file=sys.stderr)
+
+    expansions = {}
+
+    for abbr_word in abbr:
+        expansion = get_abbr_expansion_for_word(abbr_word, origin)
+        if expansion != [abbr_word]:
+            expansions[abbr_word] = expansion
+
+    return expansions
+
+def tree_split(data, parent_tree, vocabulary, abbr_expantion, key = None):
     new_tree = add_new_child(parent_tree)
     #print(new_tree.parent.nodetype)
     # 决定新树的nodetype
@@ -128,24 +186,40 @@ def tree_split(data, parent_tree, vocabulary, key = None):
             new_tree.nodetype = data['type']
         #print(data,'\n')
         vocabulary[1].add(new_tree.nodetype)
+
+        # expand those abbreviations short for their type(Class) names
+        # when they're declared
+        try:
+            if new_tree.nodetype == 'VariableDeclarationExpr':
+                for variable in data['variables']:
+                    if variable['type']['type'] == 'ClassOrInterfaceType':
+                        class_name = variable['type']['name']['identifier']
+                        variable_name = variable['name']['identifier']
+                        expansion = get_abbr_expansion(variable_name, class_name)
+                        for key, value in expansion.items():
+                            abbr_expansion[key] = value
+        except:
+            json.dump(data, sys.stderr, indent=2)
+            raise
+
         for key, data_key in data.items():            
             if key in ['comment','type']:
                 pass
             else:
-                tree_split(data_key, new_tree, vocabulary, key = key)
+                tree_split(data_key, new_tree, vocabulary, abbr_expantion, key = key)
     
     if type(data) == list:
         new_tree.nodetype = key
         vocabulary[1].add(key)
         for dataitem in data:
-            tree_split(dataitem, new_tree, vocabulary)
+            tree_split(dataitem, new_tree, vocabulary, abbr_expantion)
 
     if type(data) == str:
         new_tree.nodetype = 'string'
         if key in ['identifier', 'operator']:
             words = combine_name_split(data)
             for word in words:
-                tree_split(word, new_tree, vocabulary)
+                tree_split(word, new_tree, vocabulary, abbr_expantion)
         elif key == 'isVarArgs':
             pass
         else:
@@ -184,6 +258,7 @@ if __name__ == '__main__':
             json_data = json.load(f)
         visit_data(json_data, s)
 
+    total_abbr = 0
     for file in sorted(success):
         print('***** %s *****' % file)
         with open(os.path.join(path, file)) as f:
@@ -192,7 +267,13 @@ if __name__ == '__main__':
         voc2 = set()
         voc2.add('root')
         voc = [voc1, voc2]
+        abbr_expansion = {}
         root = Tree()
         root.nodetype = 'root'
-        tree_split(json_data, root, voc)
-        print(voc)
+        tree_split(json_data, root, voc, abbr_expansion)
+        print(voc[0])
+        print(abbr_expansion)
+        if abbr_expansion:
+            total_abbr += len(abbr_expansion)
+            print(abbr_expansion, file=sys.stderr)
+    print('total_abbr = {}'.format(total_abbr), file=sys.stderr)
